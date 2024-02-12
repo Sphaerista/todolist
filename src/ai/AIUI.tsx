6dd7eddece8e59e7b9af416172712fc91ddba1a8
Sphaerista <cosaleks@gmail.com>
2024-02-12 13:04:28 +0300

    edit chat tokens

--- src/ai/AIUI.tsx
+++ src/ai/AIUI.tsx
@@ -52,7 +52,6 @@ const AIUI: React.FC<AIUIProps> = (props) => {
   useEffect(() => {
     divRef.current?.scrollIntoView({ behavior: "smooth", block: "end" });
   });
-  const token: number = 100;
   // fetch
   const fetchData = async () => {
     try {
@@ -72,7 +71,7 @@ const AIUI: React.FC<AIUIProps> = (props) => {
                 content: sendText,
               },
             ],
-            max_tokens: { token },
+            max_tokens: 102,
           }),
         }
       );
